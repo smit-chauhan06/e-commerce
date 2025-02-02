4d77a8d31ed50057e18040ec9e9387ed1becb058
smit.groovy <smit.chauhan@groovyweb.co>
2025-02-02 21:42:31 +0530

    Add : redux setup using redux-toolkit

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import CHeader from "../components/CHeader/index";
+import { useAppDispatch } from "../store";
+import { setAccessToken } from "../store/authSlice";
 
 const Home = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setAccessToken("smit"));
+    console.log("hello");
+  }, []);
   return (
     <div>
       <CHeader />
